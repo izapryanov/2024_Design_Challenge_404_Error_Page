7a3cd4b4d10044232552d47078392d0b0ed7adaf
izapryanov <izapryanov@gmail.com>
2024-11-03 10:47:25 +0200

    Car image added, all centered.

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function openNewformChallengeURL() {
 
 document.addEventListener("DOMContentLoaded", function () {
     const carImage = document.getElementById("car-image");
-    const carSound = document.getElementById("car-sound");
-
+    // const carSound = document.getElementById("car-sound");
+    console.log(carImage);
     // Trigger animation and sound on page load
-    carSound.play();
+    // carSound.play();
 
     carImage.animate([
-        { transform: 'translateX(0px)' },
-        { transform: 'translateX(100vw)' } // Moves across the screen
+        // { transform: 'translateX(0px)' },
+        // { transform: 'translateX(100vw)' } // Moves across the screen
     ], {
-        duration: 3000, // Adjust speed
-        easing: 'ease-in-out'
+        // duration: 3000, // Adjust speed
+        // easing: 'ease-in-out'
     });
 });
